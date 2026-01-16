15ed4e9af50023cb8838f0dd11d2a23c71d61070
Nawid Adel <nawid@MacBook-Air-von-Nawid.local>
2026-01-16 16:52:48 +0100

    h1 final

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -2,19 +2,10 @@ package h1;
 
 public class H1_main {
 	public static void main(String[] args) {
-		Node a = new Node(null);
-		Node b = new Node(null);
-		Node c = new Node(null);
-		Node d = new Node(null);
-		Node e = new Node(null);
-		a.setNext(b);
-		b.setNext(c);
-		c.setNext(d);
-		d.setNext(e);
-		System.out.println(distance(b,a));
+		
 	}
 
-	public static int distance(Node x, Node y) {
+	public static int distance(Node x, Node y) {	
 		if (x == y) {
 			return 0;
 		}
